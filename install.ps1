cabba4d4355659546b880195f2aeb71fbb9a0c91
niltor <zpty@outlook.com>
2022-03-30 11:54:17 +0800

    添加nuget包支持

---
+++ install.ps1
@@ -0,0 +1,15 @@
+try {
+    # get package info
+    $VersionNode = Select-Xml -Path ./Pack.csproj -XPath '/Project//PropertyGroup/PackageVersion'
+    $PackageNode = Select-Xml -Path ./Pack.csproj -XPath '/Project//PropertyGroup/PackageId'
+    $Version = $VersionNode.Node.InnerText
+    $PackageId = $PackageNode.Node.InnerText
+
+    #re install package
+    dotnet new --uninstall $PackageId
+    dotnet new -i .\nuget\$PackageId.$Version.nupkg
+}
+catch {
+    Write-Host $_.Exception.Message
+}
+
