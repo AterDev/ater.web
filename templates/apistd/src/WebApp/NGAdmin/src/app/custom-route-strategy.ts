e9b74f6d45d9db61534785c6d43772424fc91084
niltor <zpty@outlook.com>
2022-06-29 20:13:55 +0800

    add custom-route-strategy

---
+++ templates/apistd/src/WebApp/NGAdmin/src/app/custom-route-strategy.ts
@@ -0,0 +1,11 @@
+import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from "@angular/router";
+
+export class CustomRouteReuseStrategy extends RouteReuseStrategy {
+  public shouldDetach(route: ActivatedRouteSnapshot): boolean { return false; }
+  public store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void { }
+  public shouldAttach(route: ActivatedRouteSnapshot): boolean { return false; }
+  public retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle { return {}; }
+  public shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    return (future.routeConfig === curr.routeConfig) || future.data['reuse'];
+  }
+}
