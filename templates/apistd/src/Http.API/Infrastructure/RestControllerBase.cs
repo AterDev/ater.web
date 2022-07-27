2fbd9baef4493fcb4078ab8b3a9535dc588d5550
niltor <zpty@outlook.com>
2022-07-27 16:42:31 +0800

    update restcontrollerBase

--- templates/apistd/src/Http.API/Infrastructure/RestControllerBase.cs
+++ templates/apistd/src/Http.API/Infrastructure/RestControllerBase.cs
@@ -4,12 +4,9 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 namespace Http.API.Infrastructure;
 
 /// <summary>
-/// http api 基类，重写ControllerBase中的方法
+/// 带Manager的控制器
 /// </summary>
-[ApiController]
-[Route("api/[controller]")]
-[Authorize("User")]
-public class RestControllerBase<TManager> : ControllerBase
+public class RestControllerBase<TManager> : RestControllerBase
      where TManager : class
 {
     protected readonly TManager manager;
@@ -25,6 +22,16 @@ public class RestControllerBase<TManager> : ControllerBase
         _user = user;
         _logger = logger;
     }
+
+}
+/// <summary>
+/// http api 基类，重写ControllerBase中的方法
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+[Authorize("User")]
+public class RestControllerBase : ControllerBase
+{
 
     /// <summary>
     /// 404返回格式处理
@@ -61,5 +68,4 @@ public class RestControllerBase<TManager> : ControllerBase
         };
         return base.Conflict(res);
     }
-
 }
