bfdb2ce90f6eab135ddaae7f746a5cc0b7330fe7
niltor <zpty@outlook.com>
2022-09-19 16:23:02 +0800

    use ILogger instead of Console.Writeline

--- templates/apistd/src/Application/Services/InitDataTask.cs
+++ templates/apistd/src/Application/Services/InitDataTask.cs
@@ -1,31 +1,42 @@
 ﻿namespace Application.Services;
-
 public class InitDataTask
 {
     public static async Task InitDataAsync(IServiceProvider provider)
     {
-        var context = provider.GetRequiredService<ContextBase>();
+        var context = provider.GetRequiredService<CommandDbContext>();
+        var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger<InitDataTask>();
+
+        var connectionString = context.Database.GetConnectionString();
+        logger.LogInformation("当前数据库:" + connectionString);
+
         try
         {
-            // 判断是否初始化
-            var role = await context.Roles.SingleOrDefaultAsync(r => r.Name.ToLower() == "admin");
-            if (role == null)
+            if (!await context.Database.CanConnectAsync())
             {
-                Console.WriteLine("初始化数据");
-                await InitRoleAndUserAsync(context);
+                logger.LogError("数据库无法连接，请先配置数据库！");
+            }
+            else
+            {
+                // 判断是否初始化
+                var role = await context.Roles.SingleOrDefaultAsync(r => r.Name.ToLower() == "admin");
+                if (role == null)
+                {
+                    logger.LogInformation("初始化数据");
+                    await InitRoleAndUserAsync(context);
+                }
             }
         }
         catch (Exception ex)
         {
-
-            Console.WriteLine("请检查数据库配置:" + ex.Message);
+            logger.LogError("初始化异常,请检查数据库配置：" + ex.Message);
         }
     }
 
     /// <summary>
     /// 初始化角色和管理用户
     /// </summary>
-    public static async Task InitRoleAndUserAsync(ContextBase context)
+    public static async Task InitRoleAndUserAsync(CommandDbContext context)
     {
         var role = new Role()
         {
