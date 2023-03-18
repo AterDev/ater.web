98c7e008b00d5d23bd8de563e78300743179320b
niltor <zpty@outlook.com>
2023-03-18 18:17:21 +0800

    update

--- templates/apistd/src/Http.API/Middleware/SwaggerFilter.cs
+++ templates/apistd/src/Http.API/Middleware/SwaggerFilter.cs
@@ -14,16 +14,32 @@ public class EnumSchemaFilter : ISchemaFilter
     {
         if (context.Type.IsEnum)
         {
-            //model.Enum.Clear();
-            //model.Description = "desp";
-            OpenApiArray name = new();
-            Enum.GetNames(context.Type)
-                .ToList()
-                .ForEach(n =>
+            var name = new OpenApiArray();
+            var enumData = new OpenApiArray();
+            var fields = context.Type.GetFields();
+            foreach (var f in fields)
+            {
+                if (f.Name != "value__")
                 {
-                    name.Add(new OpenApiString(n));
-                });
+                    name.Add(new OpenApiString(f.Name));
+                    var desAttr = f.CustomAttributes.Where(a => a.AttributeType.Name == "DescriptionAttribute").FirstOrDefault();
+
+                    if (desAttr != null)
+                    {
+                        var des = desAttr.ConstructorArguments.FirstOrDefault();
+                        if (des.Value != null)
+                        {
+                            enumData.Add(new OpenApiObject()
+                            {
+                                ["name"] = new OpenApiString(f.Name),
+                                ["description"] = new OpenApiString(des.Value.ToString())
+                            });
+                        }
+                    }
+                }
+            }
             model.Extensions.Add("x-enumNames", name);
+            model.Extensions.Add("x-enumData", enumData);
         }
     }
 }
