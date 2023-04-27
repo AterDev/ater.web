497b9ec656237873c692a41b4555fa3961cf486a
niltor <zpty@outlook.com>
2023-04-27 15:40:20 +0800

    update

--- templates/apistd/src/Core/Const/Const.cs
+++ templates/apistd/src/Core/Const/Const.cs
@@ -15,19 +15,3 @@ public static class Const
     /// </summary>
     public const string Version = "Version";
 }
-
-/// <summary>
-/// 错误信息
-/// </summary>
-public static class ErrorMsg
-{
-    /// <summary>
-    /// 未找到该用户
-    /// </summary>
-    public const string NotFoundUser = "未找到该用户!";
-    /// <summary>
-    /// 未找到的资源
-    /// </summary>
-    public const string NotFoundResource = "未找到的资源!";
-
-}
