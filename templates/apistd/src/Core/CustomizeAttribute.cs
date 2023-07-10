fe6f93737456dfcb448bf28a4c531caebb40ad7c
niltor <zpty@outlook.com>
2023-07-10 10:36:16 +0800

    remove NgPageAttribute

--- templates/apistd/src/Core/CustomizeAttribute.cs
+++ templates/apistd/src/Core/CustomizeAttribute.cs
@@ -1,27 +1,5 @@
 ﻿namespace Core;
 /// <summary>
-/// Angular page 生成标记
-/// </summary>
-[AttributeUsage(AttributeTargets.Class)]
-public class NgPageAttribute : Attribute
-{
-    /// <summary>
-    /// 所属模块
-    /// </summary>
-    public string Module { get; init; }
-    /// <summary>
-    /// 路由,会默认添加模块名作为前缀
-    /// </summary>
-    public string Route { get; init; }
-
-    public NgPageAttribute(string module, string route)
-    {
-        Module = module;
-        Route = route;
-    }
-}
-
-/// <summary>
 /// 模块标记
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
