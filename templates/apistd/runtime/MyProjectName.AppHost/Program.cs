2973f88b85e1c18fabb016cd9c0941233f159dcc
niltor <zpty@outlook.com>
2023-12-21 16:05:56 +0800

    update  project

--- templates/apistd/runtime/MyProjectName.AppHost/Program.cs
+++ templates/apistd/runtime/MyProjectName.AppHost/Program.cs
@@ -3,5 +3,6 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(ar
 builder.AddProject<Projects.Http_API>("http.api");
 
 // add other services
+// builder.AddProject("service", "../../src/Microservice/StandaloneService/StandaloneService.csproj");
 
 builder.Build().Run();
