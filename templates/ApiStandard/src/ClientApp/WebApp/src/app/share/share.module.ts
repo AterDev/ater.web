894948c903e74a354a569686559b882aa405b22d
niltor <zpty@outlook.com>
2024-04-25 11:31:56 +0800

    更新ng 模板

--- templates/ApiStandard/src/ClientApp/WebApp/src/app/share/share.module.ts
+++ templates/ApiStandard/src/ClientApp/WebApp/src/app/share/share.module.ts
@@ -4,6 +4,22 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ToKeyValuePipe } from './pipe/to-key-value.pipe';
 import { ComponentsModule } from '../components/components.module';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [ToKeyValuePipe],
@@ -24,3 +40,50 @@ import { ComponentsModule } from '../components/components.module';
   ]
 })
 export class ShareModule { }
+
+
+const FormModules = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatSelectModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatTooltipModule,
+  MatCheckboxModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatRadioModule,
+  ReactiveFormsModule,
+  FormsModule,
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    ...FormModules,
+
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    ...FormModules,
+  ]
+})
+export class FormModule { }
+
+
+
+
+// @NgModule({
+//   declarations: [EnumTextPipe],
+//   exports: [
+//     EnumTextPipe
+//   ]
+// })
+// export class EnumPipeModule { }
