6916412810c710ae1961c28ea2decb8137708485
niltor <zpty@outlook.com>
2024-07-02 17:52:03 +0800

    增加`ProcessHelper`执行命令的新方法

--- templates/ApiStandard/src/Infrastructure/Ater.Web.Core/Utils/ProcessHelper.cs
+++ templates/ApiStandard/src/Infrastructure/Ater.Web.Core/Utils/ProcessHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace Ater.Web.Core.Utils;
@@ -124,5 +126,67 @@ public static class ProcessHelper
         {
             return output;
         }
+    }
+
+
+    /// <summary>
+    /// 执行命令
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <param name="commands"></param>
+    /// <param name="workingDirectory"></param>
+    /// <param name="environmentVariables"></param>
+    /// <returns></returns>
+    public static string ExecuteCommands(string fileName, string[] commands, string? workingDirectory = null, StringDictionary? environmentVariables = null)
+    {
+        var commandString = string.Join(" && ", commands);
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = commandString,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        if (workingDirectory is not null)
+        {
+            process.StartInfo.WorkingDirectory = workingDirectory;
+        }
+        if (environmentVariables is not null)
+        {
+            foreach (DictionaryEntry entry in environmentVariables)
+            {
+                if (entry.Key != null)
+                {
+                    process.StartInfo.EnvironmentVariables.Add(entry.Key.ToString()!, entry.Value!.ToString());
+                }
+            }
+        }
+        var outputBuilder = new StringBuilder();
+
+        process.OutputDataReceived += (sender, e) =>
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                outputBuilder.AppendLine(e.Data);
+            }
+        };
+
+        process.ErrorDataReceived += (sender, e) =>
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                outputBuilder.AppendLine(e.Data);
+            }
+        };
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+        return outputBuilder.ToString();
     }
 }
