8352057c51378062334af186bfd45b089941e13e
niltor <zpty@outlook.com>
2024-08-15 17:26:55 +0800

    add settingContext

---
+++ templates/ApiStandard/src/Application/Implement/SettingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Application.Implement;
+
+/// <summary>
+/// 配置上下文
+/// </summary>
+public class SettingContext : ISettingContext
+{
+    private readonly IConfiguration _configuration;
+    public SettingContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        // TODO:获取需要全局使用的配置值
+    }
+
+    public T? GetValue<T>(string key)
+    {
+        return _configuration.GetValue<T>(key);
+    }
+}
---
+++ templates/ApiStandard/src/Infrastructure/Ater.Web.Abstraction/Interface/ISettingContextBase.cs
@@ -0,0 +1,16 @@
+﻿namespace Ater.Web.Abstraction.Interface;
+/// <summary>
+/// 配置文件上下文
+/// 不支持动态读取
+/// </summary>
+public interface ISettingContextBase
+{
+    /// <summary>
+    /// 获取值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    T? GetValue<T>(string key);
+
+}
